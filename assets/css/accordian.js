07ae9f263650ac327817003598b11cdf6555dd8b
farhanyp <36284284+farhanyp@users.noreply.github.com>
2024-03-12 14:43:43 +0700

    fix transition

--- assets/css/accordian.js
+++ assets/css/accordian.js
@@ -1,9 +1,20 @@
 function toggleAccordion(accordionId) {
-    const accordionCard = document.getElementById(accordionId);
-    const accordionContent = accordionCard.querySelector('.accordion-content');
-    const accordionButtonImg = accordionCard.querySelector('button img');
+  const accordionCard = document.getElementById(accordionId);
+  const accordionContent = accordionCard.querySelector('.accordion-content');
+  const accordionButtonImg = accordionCard.querySelector('button img');
   
-    accordionCard.classList.toggle('active');
-    accordionContent.style.maxHeight = accordionCard.classList.contains('active') ? "200"+ 'px' : '0';
-    accordionButtonImg.style.transform = accordionCard.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0)';
-  }
+  const wasActive = accordionCard.classList.contains('active');
+
+  accordionCard.classList.remove('active');
+
+  accordionContent.classList.remove('pt-5');
+  accordionContent.classList.remove('pt-0');
+
+  accordionCard.classList.toggle('active', !wasActive);
+
+  accordionContent.classList.toggle('pt-5', accordionCard.classList.contains('active'));
+  accordionContent.classList.toggle('pt-0', !accordionCard.classList.contains('active'));
+
+  accordionContent.style.maxHeight = accordionCard.classList.contains('active') ? "200px" : '0';
+  accordionButtonImg.style.transform = accordionCard.classList.contains('active') ? 'rotate(180deg)' : 'rotate(0)';
+}
